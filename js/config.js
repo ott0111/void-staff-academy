0084b918d1932a5d2c467a42513c888e8b2ed649
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-01 14:44:09 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// Configuration
-const CONFIG = {
-    API_BASE_URL: 'https://mod-application-backend.onrender.com',
-    TOTAL_QUESTIONS: 8,
-    PASSING_SCORE: 6
-};
-
-// Make config globally available
-window.CONFIG = CONFIG;
